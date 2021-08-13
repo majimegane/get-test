8e894e59d3bee2b5b65e0b5abfb056b55c99e595
kolosama <1274521233@qq.com>
2021-08-13 16:33:10 +0800

    把本地工程、添加到git

---
+++ src/com/itheima/git/FirstClass.java
@@ -0,0 +1,11 @@
+package com.itheima.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("呜呼起飞 ");
+        System.out.println("呜呼起飞 ");
+        System.out.println("呜呼起飞 ");
+        System.out.println("呜呼起飞 ");
+        System.out.println("呜呼起飞 ");
+    }
+}
